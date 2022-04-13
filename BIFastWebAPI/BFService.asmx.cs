e2d01265ff16404819e83e363d62963c487fcb99
Puntodewo <m.fadhillah@bankraya.co.id>
2022-04-13 08:13:55 +0700

    WIP Interface Service

--- BIFastWebAPI/BFService.asmx.cs
+++ BIFastWebAPI/BFService.asmx.cs
@@ -1,12 +1,5 @@
 ﻿using BIFastWebAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Services;
-using BIFastWebAPI.Data;
-using BIFastWebAPI.Models;
-using BIFastWebAPI.Controllers;
 
 namespace BIFastWebAPI
 {
@@ -20,12 +13,10 @@ namespace BIFastWebAPI
         [WebMethod(MessageName = "Transaction", Description = "Transaction ATM & HP")]
         public ViewModelTransaction Transaction(ViewModelTransaction data)
         {
-            string rekSbr, BankDest, NoTuj, NoRekTuj, NomTf, Desk;
-            string result = "";
+            ViewModelTransaction vmTrx = new ViewModelTransaction();
+            vmTrx = data;
 
-
-
-            return vmTf;
+            return vmTrx;
         }
 
     }
